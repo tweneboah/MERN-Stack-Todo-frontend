40f6e601db45e548da2a6a0ec9d757497c8d1924
tweneboah <twentekghana@gmail.com>
2021-05-29 20:30:56 +0800

    starter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/traditional/store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
